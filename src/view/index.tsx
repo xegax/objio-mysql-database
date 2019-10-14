4a563d38aaf2c9dbc99195b0653e6b3220107a9a
xega <xxegax@gmail.com>
2019-10-14 09:21:16 +0300

    svg icon

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -3,19 +3,19 @@ import { registerViews, OBJIOItemClassViewable } from 'objio-object/view/config'
 import { Props as ConnProps, ConnectionView } from 'objio-object/view/connection-view';
 import { Connection } from '../client/connection';
 import 'ts-react-ui/typings';
-import { Icon } from 'ts-react-ui/icon';
 import * as ConnIcon from '../images/mysql-connection.svg';
 import * as DBIcon from '../images/mysql-database.svg';
 import { ObjectToCreate }  from 'objio-object/common/interfaces';
 import { DatabaseHolder } from 'objio-object/client/database/database-holder';
 import { Database } from '../client/database';
+import { IconSVG } from 'ts-react-ui/icon-svg';
 
 export function getObjectsToCreate(): Array<ObjectToCreate> {
   return [
     {
       name: 'mysql',
       desc: 'mysql database',
-      icon: <Icon src={DBIcon}/>,
+      icon: <IconSVG icon={DBIcon}/>,
       create: () => new DatabaseHolder({ impl: new Database() })
     }, {
       name: 'mysql connection',
@@ -32,7 +32,7 @@ export function getViews(): Array<OBJIOItemClassViewable> {
       view: (props: ConnProps) => <ConnectionView {...props}/>
     }],
     desc: 'Mysql connection',
-    icons: { item:  <Icon src={ConnIcon}/> }
+    icons: { item:  <IconSVG icon={ConnIcon}/> }
   });
 
   return [
